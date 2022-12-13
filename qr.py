13f8c9223e9e11dd3527d9d1978af5b0d408ae9e
NidaZORLU <nidazorlunz@hotmail.com>
2022-12-13 16:56:05 +0300

    add new code

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-
+import qr
 def open_file():
     filepath = askopenfilename(
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
@@ -25,6 +25,11 @@ def save_file():
         output_file.write(text)
     window.title(f"QR System - {filepath}")
 
+def delete_label():
+    window.destroy()
+  
+
+
 window = tk.Tk()
 window.title("QR System")
 window.rowconfigure(0, minsize=480, weight=1)
@@ -34,9 +39,12 @@ txt_edit = tk.Text(window)
 fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
 btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
+btn_delete=tk.Button(fr_buttons, text="Delete",command=delete_label)
+
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+btn_delete.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
 
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
